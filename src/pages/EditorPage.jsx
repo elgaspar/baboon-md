a345c5a522bcd37b75b7f84079d171c9f2101589
Elias Gasparis <iliasgas@gmail.com>
2025-10-09 20:03:46 +0300

    styling

--- src/pages/EditorPage.jsx
+++ src/pages/EditorPage.jsx
@@ -8,7 +8,7 @@ export default function EditorPage() {
         <div className="flex flex-col min-h-screen bg-white text-gray-900 overflow-hidden">
             <Navbar/>
 
-            <main className="flex-1 flex flex-col m-11 mb-3 border border-gray-300 rounded-2xl shadow-sm overflow-hidden">
+            <main className="flex-1 flex flex-col m-11 mb-3 border border-gray-300 rounded-lg shadow-sm overflow-hidden">
                 <HeaderBar />
                 <EditorAndPreview/>
             </main>
