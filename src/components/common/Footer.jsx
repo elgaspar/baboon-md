4374659d262911d1c5735c57dce86db2781894c7
Elias Gasparis <iliasgas@gmail.com>
2025-10-09 18:49:37 +0300

    changed footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -2,16 +2,18 @@ export default function Footer() {
     const year = new Date().getFullYear();
 
     return (
-        <footer className=" mb-3 text-gray-500 text-sm text-center px-4">
-            © {year} BaboonMD ·{" "}
+        <footer className="mb-3 text-gray-500 text-sm text-center px-4">
+            Crafted with ❤️ & ☕ by{" "}
             <a
                 href="https://github.com/elgaspar"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="underline hover:opacity-80"
+                className="hover:underline hover:opacity-80"
             >
                 Elias Gasparis
             </a>
+            <span className="mx-2">·</span>
+            © {year} BaboonMD
         </footer>
     );
 }
