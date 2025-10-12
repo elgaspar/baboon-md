419cc73a171082f8be52b5f6aafc1a2695962421
Elias Gasparis <iliasgas@gmail.com>
2025-10-12 20:12:45 +0300

    wip

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 export default defineConfig({
     server: {
-        allowedHosts: ["host.docker.internal"],
+        allowedHosts: ["host.docker.internal", "frontend"],
     },
     plugins: [
         react(),
