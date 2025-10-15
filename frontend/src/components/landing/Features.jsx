789d6522393d02d05cb9a0f03b259b97ca837751
Elias Gasparis <iliasgas@gmail.com>
2025-10-15 18:55:04 +0300

    fixed styling

--- frontend/src/components/landing/Features.jsx
+++ frontend/src/components/landing/Features.jsx
@@ -29,7 +29,7 @@ export default function Features() {
             <div className="grid grid-cols-1 md:grid-cols-3 gap-10 text-center md:text-left">
                 {features.map(({ icon: Icon, title, description }) => (
                     <div className="p-4 rounded-xl transition hover:bg-gray-50 mx-auto md:mx-0" key={title}>
-                        <div className="flex flex-col md:flex-row items-center md:items-start gap-2 mb-3 text-accent">
+                        <div className="flex flex-col md:flex-row items-center gap-2 mb-3 text-accent">
                             <div className="bg-accent/10 p-2 rounded-full">
                                 <Icon className="w-5 h-5 text-accent" />
                             </div>
