74b1e69f096242837e0ad5c010c00b32f0b8a352
Elias Gasparis <iliasgas@gmail.com>
2025-11-01 15:04:06 +0200

    fixed e2e workflow

--- tests/playwright.config.prod.js
+++ tests/playwright.config.prod.js
@@ -6,6 +6,6 @@ export default defineConfig({
   ...baseConfig,
   webServer: {
     ...baseConfig.webServer,
-    command: 'docker compose -f ../docker-compose.prod.yml up',
+    command: 'docker compose -f ../docker-compose.prod.yml --env-file ../.env.prod up',
   },
 });
